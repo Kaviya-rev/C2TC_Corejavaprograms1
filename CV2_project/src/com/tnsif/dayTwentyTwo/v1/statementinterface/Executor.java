a6c5852089e1ffe5fe4ad63d1439959e6c597203
kaviya-rev <kavirkaviyad12344@gmail.com>
2025-09-01 15:42:50 +0530

    DAY22 COMPLETED

---
+++ CV2_project/src/com/tnsif/dayTwentyTwo/v1/statementinterface/Executor.java
@@ -0,0 +1,22 @@
+package com.tnsif.dayTwentyTwo.v1.statementinterface;
+
+//Program to demonstrate Statement Interface
+
+public class Executor {
+
+	public static void main(String[] args) {
+		UsingStatementInterface.showEmp();
+		System.out.println("----------------------------------------------");
+		System.out.println(UsingStatementInterface.updateEmpName(5, "Neha"));
+		UsingStatementInterface.updateEmpSalary(5, 65000);
+		System.out.println("-------------After Updating Name and Salary------------");
+		UsingStatementInterface.showEmp();
+		UsingStatementInterface.deleteEmp(5);
+		UsingStatementInterface.deleteEmp(101);
+		UsingStatementInterface.deleteEmp(102);
+		System.out.println("-----------After Deleting --------------------");
+		UsingStatementInterface.showEmp();
+
+	}
+
+}
